e8010d3b80f3627d007de12e40bb0f9645647fc7
Christer Carlsson <ccarlsson@outlook.com>
2019-01-21 11:53:38 +0100

    Add Book Entites

---
+++ BookDB.Api/Data/Entites/BookCopy.cs
@@ -0,0 +1,9 @@
+namespace BookDB.Api
+{
+    public class BookCopy
+    {
+        public string BookId { get; set; }
+        public int CopyNumber { get; set; }
+        public string Description { get; set; }
+    }
+}
