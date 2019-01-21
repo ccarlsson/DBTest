9da2e5c88640a2279806f235d6cd2dde50e1a9ab
Christer Carlsson <ccarlsson@outlook.com>
2019-01-21 14:52:31 +0100

    'test'

--- BookDB.Api/Controllers/BooksController.cs
+++ BookDB.Api/Controllers/BooksController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BookDB.Api.Entites;
 using BookDB.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
